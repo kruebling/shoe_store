d40a2a9e2f0c15eadf95621e06a2e97302956abd
kr <keegan.ruebling@gmail.com>
2017-09-29 09:50:06 -0700

    pass initial relationship specs

--- db/migrate/20170929164155_create_brands.rb
+++ db/migrate/20170929164155_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[5.1]
   def change
     create_table(:brands) do |t|
-      t.column(:name, :string)
+      t.column(:brand, :string)
       t.column(:price, :integer)
 
     end
