24df0c00c6a44e516fb1c69ecfe39f8c771cdbab
kr <keegan.ruebling@gmail.com>
2017-09-29 12:08:03 -0700

    add styling to info page. methods for renaming and delete as well

--- app.rb
+++ app.rb
@@ -42,3 +42,34 @@ end
 #End Index Routes
 
 #Info Routes
+get('/store/:id') do
+  @store = Store.find(params[:id])
+  @brands = Brand.all
+  @unused_brands = @brands - @store.brands
+  erb(:store_info)
+end
+
+post('/add_brand/:id') do
+  store = Store.find(params[:id])
+  brands = Brand.all
+  brand_ids = params['brand_ids']
+  brand_ids.each do |brand_id|
+    store.brands.push(Brand.find(brand_id))
+  end
+  redirect "/store/#{store.id}"
+end
+
+delete("/store/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  @store.delete
+  @stores = Store.all()
+  redirect "/"
+end
+
+patch("/store/:id") do
+  rename = params.fetch('rename')
+  @Store = Store.find(params.fetch("id").to_i())
+  @Store.update({:name => rename})
+  @Store = Store.all()
+  redirect '/'
+end
