93efbd982b40980c4d58d858c850043008f8d9a8
kr <keegan.ruebling@gmail.com>
2017-09-29 16:06:27 -0700

    add passing specs for validation

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores)}
+
+  it('limites the length of input to 100 characters') do
+    brand = Brand.new({:brand => 'This title is too long'.*(5)})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it('prevents duplicate entries') do
+    Brand.create({:brand => 'Dupli Cate'})
+    brand = Brand.new({:brand => 'Dupli Cate'})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it('capitalizes the title') do
+    brand = Brand.new({:brand => 'test'})
+    brand.save()
+    expect(brand.brand).to(eq('Test'))
+  end
+
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands)}
+
+  it('limites the length of input to 100 characters') do
+    store = Store.new({:name => 'This title is too long'.*(5)})
+    expect(store.save()).to(eq(false))
+  end
+
+  it('prevents duplicate entries') do
+    Store.create({:name => 'Dupli Cate'})
+    store = Store.new({:name => 'Dupli Cate'})
+    expect(store.save()).to(eq(false))
+  end
+
+  it('capitalizes the title') do
+    store = Store.new({:name => 'testing'})
+    store.save()
+    expect(store.name).to(eq('Testing'))
+  end
 end
